5416c4802756a9a62c624f4fbdf534303cd42700
CristianLechon <calechon@uce.edu.ec>
2025-07-07 13:00:49 -0500

    Sumativa final

---
+++ G04-ESF/app/src/main/java/com/example/app_vehiculos/data/local/UsuarioDao.kt
@@ -0,0 +1,33 @@
+package com.example.app_vehiculos.data.local
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface UsuarioDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertarUsuario(usuario: Usuario)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertarUsuarios(usuarios: List<Usuario>)
+
+    @Query("SELECT * FROM usuarios WHERE nombre = :nombre LIMIT 1")
+    suspend fun getUsuarioPorNombre(nombre: String): Usuario?
+
+    @Query("SELECT COUNT(*) FROM usuarios")
+    suspend fun countUsuarios(): Int
+
+    @Insert
+    suspend fun insertarUsuarioYDevolverId(usuario: Usuario): Long
+
+    @Query("SELECT * FROM usuarios")
+    fun getAllUsuarios(): Flow<List<Usuario>>
+
+    @Query("UPDATE usuarios SET lastLogin = :fecha WHERE nombre = :nombre")
+    suspend fun actualizarLastLogin(nombre: String, fecha: String)
+}
+
